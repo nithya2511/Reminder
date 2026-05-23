a6835bf087cf402271b95761551faa250a0831ac
Nithya Vasudevan <nithya1vasudevan@gmail.com>
2026-05-23 16:52:20 +0200

    Initial Commit

---
+++ Reminder/ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderApp.swift
+//  Reminder
+//
+//  Created by Nithya Vasudevan on 23.05.26.
+//
+
+import SwiftUI
+
+@main
+struct ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
