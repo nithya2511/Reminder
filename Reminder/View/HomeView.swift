bceef78f7dc292135f265f1e765b348c26470721
Nithya Vasudevan <nithya1vasudevan@gmail.com>
2026-05-23 16:59:18 +0200

    Initial Commit

---
+++ Reminder/View/HomeView.swift
@@ -0,0 +1,14 @@
+//
+//  HomeView.swift
+//  Reminder
+//
+//  Created by Nithya Vasudevan on 23.05.26.
+//
+
+import SwiftUI
+
+struct HomeView : View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
